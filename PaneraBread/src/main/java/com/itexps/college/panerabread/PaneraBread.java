fcf68d68c4325e83feb3f29bc5d2b7812ff2f21d
suvarna1982 <suvarnarode1@gmail.com>
2024-12-18 12:49:17 -0600

    Add files via upload

---
+++ PaneraBread/src/main/java/com/itexps/college/panerabread/PaneraBread.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexps.college.panerabread;
+
+/**
+ *
+ * @author adity
+ */
+public class PaneraBread {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
